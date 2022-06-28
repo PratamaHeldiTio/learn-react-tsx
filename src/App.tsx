625a768e71ae3963bcdd01cd2d673578f2fd5d18
pratamahelditio <pratamahelditio@gmail.com>
2022-06-28 17:14:07 +0700

    react-router and refactor folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import YoutubePage from './youtube';
-import ProductPage from './product';
-import BlogPostPage from './blogpost';
-import Navbar from '../container/Navbar';
-import { DetailBlogpost } from '../container/BlogPosts/Partials';
-import NotFound from './NotFound';
+import YoutubePage from './pages/youtube';
+import ProductPage from './pages/product';
+import BlogPostPage from './pages/blogpost';
+import Navbar from './container/Navbar';
+import { DetailBlogpost } from './container/BlogPosts/Partials';
+import NotFound from './pages/NotFound';
 
 
 const HomePage = () => {
@@ -15,7 +15,10 @@ const HomePage = () => {
       <Routes>
         <Route path='/' element={<BlogPostPage />} />
         <Route path='/detail-blog/:id' element={<DetailBlogpost />} />
-        <Route path='/product' element={<ProductPage />} />
+        <Route path='/product' element={<ProductPage />}>
+          <Route path='terkini' element={<YoutubePage />} />
+          <Route path='terkina' element={<ProductPage />} />
+        </Route>
         <Route path='/youtube' element={<YoutubePage />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import Product from '../container/Product';
+import { Outlet, Link } from 'react-router-dom';
 
 
 const ProductPage = () => {
-  return <Product />;
+  return (
+    <>
+      <Product />
+      <nav>
+        <Link to="terkini">terkini</Link> | <Link to="terkina">terkina</Link>
+      </nav>
+      <Outlet />
+    </>
+  );
 };
 
 export default ProductPage;
